d29f0132277b67e8969b269593e43d54522d3af2
kadgodda <business.kadeem@gmail.com>
2018-03-11 16:27:34 -0400

    Added Routes for Registration Page

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 var HomeController = require('./controllers/HomeController');
 var LoginController = require('./controllers/LoginController');
+var RegController = require('./controllers/RegController')
 
 // Routes
 module.exports = function(app){
@@ -9,4 +10,6 @@ module.exports = function(app){
     app.get('/other', HomeController.Other);
 	app.get('/login', LoginController.Login);
 	app.post('/login', LoginController.Authenticate);
+	app.get('/register', RegController.Register);
+    	app.post('/register', RegController.Submit);
 };
